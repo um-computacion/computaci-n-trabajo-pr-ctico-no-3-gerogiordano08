4f19f2eba3ab528ac338c5eb30445eccd02e585f
gerogiordano08 <85655339+gerogiordano08@users.noreply.github.com>
2025-04-16 11:48:26 -0300

    he agregado tests para verificar si al ingresar un número negativo da el error correcto

--- tests/test_calculo_numeros.py
+++ tests/test_calculo_numeros.py
@@ -22,6 +22,20 @@ class TestCalculoNumeros(unittest.TestCase):
         numero=ingrese_numero()
         self.assertEqual(numero, 100)
 
+    @patch(
+        'builtins.input',
+        return_value='-100'
+        )
+    def test_ingreso_negativo(self, patch_input):
+        with self.assertRaises(NumeroDebeSerPositivo):
+            ingrese_numero()
+    @patch(
+        'builtins.input',
+        return_value='-2'
+        )
+    def test_ingreso_negativo(self, patch_input):
+        with self.assertRaises(NumeroDebeSerPositivo):
+            ingrese_numero()
 
 if __name__ == '__main__':
     unittest.main() 

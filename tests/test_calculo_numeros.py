2c961c228dbb6d5512dd1d0151497c1b0857fa29
gerogiordano08 <85655339+gerogiordano08@users.noreply.github.com>
2025-04-16 13:03:18 -0300

    implementé las excepciones, asi terminando el trabajo practico. los tests dan OK

--- tests/test_calculo_numeros.py
+++ tests/test_calculo_numeros.py
@@ -1,57 +1,39 @@
 import unittest
-from src.exceptions import NumeroDebeSerPositivo
-from src.calculo_numeros import ingrese_numero
-
 from unittest.mock import patch
+from src.calculo_numeros import ingrese_numero
+from src.exceptions import NumeroDebeSerPositivo
 
 class TestCalculoNumeros(unittest.TestCase):
 
-    @patch(
-        'builtins.input',
-        return_value=100
-    )
+    @patch('builtins.input', return_value='100')
     def test_ingreso_feliz(self, patch_input):
-        numero = ingrese_numero()
+        numero = ingrese_numero(loop=False)
         self.assertEqual(numero, 100)
-    @patch(
-        'builtins.input',
-        return_value=55
-    )
+
+    @patch('builtins.input', return_value='55')
     def test_ingreso_bien(self, patch_input):
-        numero=ingrese_numero()
+        numero = ingrese_numero(loop=False)
         self.assertEqual(numero, 55)
 
-
-    @patch(
-        'builtins.input',
-        return_value=-100
-        )
+    @patch('builtins.input', return_value='-100')
     def test_ingreso_negativo(self, patch_input):
         with self.assertRaises(NumeroDebeSerPositivo):
-            ingrese_numero()
-    @patch(
-        'builtins.input',
-        return_value=-2
-        )
+            ingrese_numero(loop=False)
+
+    @patch('builtins.input', return_value='-2')
     def test_ingreso_negativo_2(self, patch_input):
         with self.assertRaises(NumeroDebeSerPositivo):
-            ingrese_numero()
+            ingrese_numero(loop=False)
 
-
-    @patch(
-        'builtins.input',
-        return_value='AAA'
-    )
+    @patch('builtins.input', return_value='AAA')
     def test_ingreso_letras(self, patch_input):
         with self.assertRaises(ValueError):
-            ingrese_numero()
-    @patch(
-        'builtins.input',
-        return_value='eol'
-    )
+            ingrese_numero(loop=False)
+
+    @patch('builtins.input', return_value='eol')
     def test_ingreso_letras_2(self, patch_input):
         with self.assertRaises(ValueError):
-            ingrese_numero()
+            ingrese_numero(loop=False)
 
 if __name__ == '__main__':
-    unittest.main() 
+    unittest.main()

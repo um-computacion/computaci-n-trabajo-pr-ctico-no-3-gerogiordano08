b348675f887cf12542c359c0543713d32cb0a479
gerogiordano08 <85655339+gerogiordano08@users.noreply.github.com>
2025-04-16 11:58:58 -0300

    agregue los tests para casos en los que el usuario no ingresa numeros

--- tests/test_calculo_numeros.py
+++ tests/test_calculo_numeros.py
@@ -20,7 +20,8 @@ class TestCalculoNumeros(unittest.TestCase):
     )
     def test_ingreso_bien(self, patch_input):
         numero=ingrese_numero()
-        self.assertEqual(numero, 100)
+        self.assertEqual(numero, 55)
+
 
     @patch(
         'builtins.input',
@@ -33,8 +34,24 @@ class TestCalculoNumeros(unittest.TestCase):
         'builtins.input',
         return_value='-2'
         )
-    def test_ingreso_negativo(self, patch_input):
+    def test_ingreso_negativo_2(self, patch_input):
         with self.assertRaises(NumeroDebeSerPositivo):
+            ingrese_numero()
+
+
+    @patch(
+        'builtins.input',
+        return_value='AAA'
+    )
+    def test_ingreso_letras(self, patch_input):
+        with self.assertRaises(ValueError):
+            ingrese_numero()
+    @patch(
+        'builtins.input',
+        return_value='eol'
+    )
+    def test_ingreso_letras_2(self, patch_input):
+        with self.assertRaises(ValueError):
             ingrese_numero()
 
 if __name__ == '__main__':
